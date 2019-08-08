c59f78c93c6e2ced4bebea07e8e87ea92745cdcd
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-08 23:03:25 +0100

    setup node with typescript

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,27 @@ const app: Application = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response, next: NextFunction): object => {
+app.get("/", (req: Request, res: Response): object => {
     return res.json({ status: "success", message: "Welcome to API Service" });
   }
 );
 
-app.listen(3000, () => console.log("app listening"));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = new Error("Route Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error: { message: string; status: number }, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500);
+    res.json({
+      status: "error",
+      message: error.message
+    });
+    next();
+  }
+);
+
+const PORT: any = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
